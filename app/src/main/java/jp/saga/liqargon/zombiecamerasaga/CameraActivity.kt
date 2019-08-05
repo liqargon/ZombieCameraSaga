981ee322fea52718301fff754ab2335120354c74
liqargon <liqargon@hanarchy.biz>
2019-08-05 19:53:49 +0900

    [add] bare activity following app structure

---
+++ app/src/main/java/jp/saga/liqargon/zombiecamerasaga/CameraActivity.kt
@@ -0,0 +1,12 @@
+package jp.saga.liqargon.zombiecamerasaga
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
---
+++ app/src/main/java/jp/saga/liqargon/zombiecamerasaga/FrameActivity.kt
@@ -0,0 +1,12 @@
+package jp.saga.liqargon.zombiecamerasaga
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FrameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame)
+    }
+}
