993e8823f5c4d363d3064a355c2f2567a61ca276
liqargon <liqargon@hanarchy.biz>
2019-08-05 20:40:49 +0900

    [add] camera feature

--- app/src/main/java/jp/saga/liqargon/zombiecamerasaga/CameraActivity.kt
+++ app/src/main/java/jp/saga/liqargon/zombiecamerasaga/CameraActivity.kt
@@ -1,12 +1,104 @@
 package jp.saga.liqargon.zombiecamerasaga
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Matrix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Rational
+import android.view.Surface
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class CameraActivity : AppCompatActivity() {
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+    private val REQUESTED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        viewFinder = findViewById(R.id.view_finder)
+
+        if (allPermissionGranted()){
+            viewFinder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUESTED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
     }
+
+    private lateinit var viewFinder: TextureView
+
+    private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetAspectRatio(Rational(1,1))
+        }.build()
+
+        val preview = Preview(previewConfig)
+
+        preview.setOnPreviewOutputUpdateListener {
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+        CameraX.bindToLifecycle(this, preview)
+
+    }
+
+    private fun updateTransform() {
+        val matrix = Matrix()
+
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        val rotationDegrees = when(viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        viewFinder.setTransform(matrix)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionGranted() = REQUESTED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
 }
--- app/src/main/java/jp/saga/liqargon/zombiecamerasaga/MainActivity.kt
+++ app/src/main/java/jp/saga/liqargon/zombiecamerasaga/MainActivity.kt
@@ -1,12 +1,19 @@
 package jp.saga.liqargon.zombiecamerasaga
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun launchCamera(view: View) {
+        val intent = Intent(this, CameraActivity::class.java).apply { }
+        startActivity(intent)
     }
 }
